a7c847a8e0902d15e59679c0fd6bdeb8e03a5534
NelsonWang <wangnelson2@gmail.com>
2021-05-04 03:04:01 -0400

    my react app | myTodos

--- firstProject/my-react-app/src/App.js
+++ firstProject/my-react-app/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <h1>My Todos</h1>
+      <div>
+        <h2>Title</h2>
+        <div>
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }

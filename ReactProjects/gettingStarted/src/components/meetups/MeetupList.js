4b4d5261ad2ccbeb56485626c7535e5152f3df41
NelsonWang <wangnelson2@gmail.com>
2021-05-05 19:50:56 -0400

    my react app | Netflix App | Meetup | Components Lists and Items

---
+++ ReactProjects/gettingStarted/src/components/meetups/MeetupList.js
@@ -0,0 +1,20 @@
+import MeetupItem from "./MeetupItem";
+import React from "react";
+import classes from "./MeetupList.module.css";
+
+export default function MeetupList(props) {
+  return (
+    <ul className={classes.list}>
+      {props.meetups.map((meetup) => (
+        <MeetupItem
+          key={meetup.id}
+          id={meetup.id}
+          image={meetup.image}
+          title={meetup.title}
+          address = {meetup.address}
+          description = {meetup.description}
+        />
+      ))}
+    </ul>
+  );
+}
--- ReactProjects/gettingStarted/src/pages/AllMeetups.js
+++ ReactProjects/gettingStarted/src/pages/AllMeetups.js
@@ -1,3 +1,5 @@
+
+import MeetupList from '../components/meetups/MeetupList'
 const DUMMY_DATA = [
   {
     id: 'm1',
@@ -22,9 +24,7 @@ const DUMMY_DATA = [
 function AllMeetupsPage() {
   return <section>
     <h1>All Meetups</h1>
-    {DUMMY_DATA.map((meetup) => {
-      return <li key={meetup.id}>{meetup.title}</li>
-    })}
+    <MeetupList meetups={DUMMY_DATA} />
   </section>;
 }
 

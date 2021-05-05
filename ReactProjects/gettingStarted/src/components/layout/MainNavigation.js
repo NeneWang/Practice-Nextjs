cbadd8bc3af4957fc88f4f57b08c662dc2869a5e
NelsonWang <wangnelson2@gmail.com>
2021-05-05 19:08:16 -0400

    my react app | Netflix App | Router | Main Navigation Links

--- ReactProjects/gettingStarted/src/components/layout/MainNavigation.js
+++ ReactProjects/gettingStarted/src/components/layout/MainNavigation.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function MainNavigation() {
   return (
     <header>
       <div>React Meeetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Some Link</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">Add New Meetup</Link>
+          </li>
+          <li>
+            <Link to="/favorites">Favorites</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }

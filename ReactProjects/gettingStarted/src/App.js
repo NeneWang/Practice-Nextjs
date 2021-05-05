a016ea594c9924ad55e662f152616e6127984e1b
NelsonWang <wangnelson2@gmail.com>
2021-05-05 18:53:09 -0400

    my react app | Netflix App | Router | Implementation

--- ReactProjects/gettingStarted/src/App.js
+++ ReactProjects/gettingStarted/src/App.js
@@ -1,5 +1,22 @@
+import { Route } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/NewMeetup";
+import NewMeetupPage from "./pages/Favorites";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Route path="/">
+        <AllMeetupsPage />
+      </Route>
+      <Route path="/new-meetup">
+        <NewMeetupPage />
+      </Route>
+      <Route path="/favorites">
+        <FavoritesPage />
+      </Route>
+    </div>
+  );
 }
 
 export default App;

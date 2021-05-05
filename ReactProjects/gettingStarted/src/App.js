608373a3e67e6d5668564343b8abfb0c855687db
NelsonWang <wangnelson2@gmail.com>
2021-05-05 19:09:22 -0400

    my react app | Netflix App | Router | Main Navigation Links IMPL

--- ReactProjects/gettingStarted/src/App.js
+++ ReactProjects/gettingStarted/src/App.js
@@ -3,9 +3,12 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/NewMeetup";
 import NewMeetupPage from "./pages/Favorites";
 
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />

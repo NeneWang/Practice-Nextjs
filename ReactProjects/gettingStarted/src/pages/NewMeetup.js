a6839529012e3d0f9b20473b9b3afad9d32b74e7
NelsonWang <wangnelson2@gmail.com>
2021-05-06 01:33:38 -0400

    my react app | Netflix App | Form | History Traversal

--- ReactProjects/gettingStarted/src/pages/NewMeetup.js
+++ ReactProjects/gettingStarted/src/pages/NewMeetup.js
@@ -1,17 +1,22 @@
+import { useHistory } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
+
   function addMeetupHandler(meetupData) {
     fetch(
       "https://descartable-server-default-rtdb.firebaseio.com/meetups.json",
       {
         method: "POST",
         body: JSON.stringify(meetupData),
-        headers:{
-          'Content-Type': 'appliaction/json'
-        }
+        headers: {
+          "Content-Type": "appliaction/json",
+        },
       }
-    );
+    ).then(() => {
+      history.replace('/');
+    });
   }
 
   return (

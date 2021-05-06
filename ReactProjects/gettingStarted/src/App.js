70a14db2b64296735d4575deac2ae46bb270fcb2
NelsonWang <wangnelson2@gmail.com>
2021-05-06 00:19:00 -0400

    my react app | Netflix App | Layout

--- ReactProjects/gettingStarted/src/App.js
+++ ReactProjects/gettingStarted/src/App.js
@@ -3,12 +3,10 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/NewMeetup";
 import NewMeetupPage from "./pages/Favorites";
 
-import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
-      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />
---
+++ ReactProjects/gettingStarted/src/components/layout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./Layout.module.css";
+import MainNavigation from "./MainNavigation";
+
+export default function Layout() {
+  return (
+    <div>
+      <MainNavigation />
+    </div>
+  );
+}

611eea39e9bdd1e906e0b307082384922f53ae87
NelsonWang <wangnelson2@gmail.com>
2021-05-06 02:24:45 -0400

    my react app | Netflix App | Context | Handlers Setup

--- ReactProjects/gettingStarted/src/store/favorites-context.js
+++ ReactProjects/gettingStarted/src/store/favorites-context.js
@@ -7,9 +7,24 @@ const FavoritesContext = createContext({
 
 function FavoritesContextProvider(props) {
   const [userFavorites, setUserFavorites] = useState([]);
+
+  function addFavoriteHanlder(favoriteMeetup) {
+    setUserFavorites((previousFavorites) => {
+      return userFavorites.concat(favoriteMeetup);
+    });
+  }
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+  function itemIsFavoriteHandler(meetupId) {
+      return userFavorites.some(meetup => meetup.id === meetupId );
+  }
+
   const context = {
-      favorites: userFavorites,
-      totalFavorites: userFavorites.length,
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
   };
   return (
     <FavoritesContext.Provider>{props.children}</FavoritesContext.Provider>

8bd52caa63c18e257e76c63ceae95af0e804731c
NelsonWang <wangnelson2@gmail.com>
2021-05-07 04:50:10 -0400

    PageFiledBasedRouting | Dynamic Routed Paths +

--- PagesFileBasedRouting/pages/clients/[id]/index.js
+++ PagesFileBasedRouting/pages/clients/[id]/index.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router";
 
 export default function ClientProjectsPage() {
-    return (
-        <div>
-            <h1>The Projects of a Given Client</h1>
-        </div>
-    )
+    const router = useRouter();
+  console.log(router.query);
+  return (
+    <div>
+      <h1>The Projects of a Given Client</h1>
+    </div>
+  );
 }

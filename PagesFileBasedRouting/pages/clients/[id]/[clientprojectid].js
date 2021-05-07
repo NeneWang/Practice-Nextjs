213448bae81bd8ebd60ffb120818186efdf6606a
NelsonWang <wangnelson2@gmail.com>
2021-05-07 04:44:23 -0400

    PageFiledBasedRouting | Nested Clients

---
+++ PagesFileBasedRouting/pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SelectedCleintProjectPage() {
+  return (
+    <div>
+      <h1>The Prohject Page for a Specific Project for a Selected Client</h1>
+    </div>
+  );
+}

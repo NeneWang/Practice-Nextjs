7b8eb9f83c791bced4e442df3e64d1b8e704442e
NelsonWang <wangnelson2@gmail.com>
2021-05-07 07:06:21 -0400

    FileBasedProject | Events | Event Components | Icons | Implementation

--- section4ProjectTimeStarter/components/events/event-item.js
+++ section4ProjectTimeStarter/components/events/event-item.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Link from "next/link";
 
+import Button from "../ui/button";
+
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 import classes from "./event-item.module.css";
-
-import Button from '../ui/button'
 
 export default function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -18,19 +21,26 @@ export default function EventItem(props) {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li className={classes.item} >
+    <li className={classes.item}>
       <img src={`/${image}`} alt={title} />
-      <div className={classes.content} >
-        <div className={classes.summary} >
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div className={classes.date} >
+          <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
-          <div>
-            <address className={classes.address} >{formattedAddress}</address>
+          <div className={classes.address} >
+            <AddressIcon />
+            <address className={classes.address}>{formattedAddress}</address>
           </div>
-          <div className={classes.actions} >
-            <Button link={exploreLink} >Explore Event</Button>
+          <div className={classes.actions}>
+            <Button link={exploreLink}>
+              <span>Explore Event</span>
+              <span className={classes.icon}>
+                <ArrowRightIcon />
+              </span>
+            </Button>
           </div>
         </div>
       </div>

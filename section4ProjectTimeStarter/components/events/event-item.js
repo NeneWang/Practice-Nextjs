22c092e1119a2348e46e1ef9c15ec23b60d8fead
NelsonWang <wangnelson2@gmail.com>
2021-05-07 06:41:35 -0400

    FileBasedProject | Events | Event Components | Event Item | Style added

--- section4ProjectTimeStarter/components/events/event-item.js
+++ section4ProjectTimeStarter/components/events/event-item.js
@@ -1,20 +1,21 @@
 import React from "react";
 import Link from "next/link";
 
+import classes from "./event-item.module.css";
+
 export default function EventItem(props) {
   const { title, image, date, location, id } = props;
   const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
     day: "numeric",
     month: "long",
-    year: 'numeric',
+    year: "numeric",
   });
 
   const formattedAddress = location.replace(", ", "\n");
   const exploreLink = `/events/${id}`;
 
-
   return (
-    <li>
+    <li className={classes.item} >
       <img src={`/${image}`} alt={title} />
       <div>
         <div>

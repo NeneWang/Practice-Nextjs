3e5ea1af91963110ffac8b9d0eb25f00563a6f90
NelsonWang <wangnelson2@gmail.com>
2021-05-07 05:23:28 -0400

    PageFiledBasedRouting | Clients | id | index

--- PagesFileBasedRouting/pages/clients/[id]/index.js
+++ PagesFileBasedRouting/pages/clients/[id]/index.js
@@ -4,9 +4,16 @@ import { useRouter } from "next/router";
 export default function ClientProjectsPage() {
   const router = useRouter();
   console.log(router.query);
+
+  function loadProjectHandler(){
+    //  Load Data...
+    router.push('/clients/max/projecta');
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler} >Load Project A</button>
     </div>
   );
 }

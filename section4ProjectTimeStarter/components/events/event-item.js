29c827e9519e1a26afa5aa0a4468bb9bb2d35750
NelsonWang <wangnelson2@gmail.com>
2021-05-07 06:15:18 -0400

    FileBasedProject | Events | Event Components | Event Item | humanReadable

--- section4ProjectTimeStarter/components/events/event-item.js
+++ section4ProjectTimeStarter/components/events/event-item.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 export default function EventItem(props) {
-    const {items} = props
-    return (
-        <li>
-           {items.map} 
-        </li>
-    )
+  const { title, image, date, location, id } = props;
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+      day: 'numeric',
+      month: 'long',
+      year: numeric,
+  });
+  return (
+    <li>
+      <img src="" alt="" />
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            <time>{date}</time>
+          </div>
+          <div>
+            <address>ADDRESS</address>
+          </div>
+          <div>
+            <Link href="/">Explore Event</Link>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
 }

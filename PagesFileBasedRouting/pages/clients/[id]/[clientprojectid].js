62cf010417c0ed044b41eea077d23060c35dbae6
NelsonWang <wangnelson2@gmail.com>
2021-05-07 04:47:59 -0400

    PageFiledBasedRouting | Dynamic Routed Paths

--- PagesFileBasedRouting/pages/clients/[id]/[clientprojectid].js
+++ PagesFileBasedRouting/pages/clients/[id]/[clientprojectid].js
@@ -1,6 +1,10 @@
 import React from "react";
 
+import { useRouter } from "next/router";
+
 export default function SelectedCleintProjectPage() {
+  const router = useRouter();
+  console.log(router.query);
   return (
     <div>
       <h1>The Prohject Page for a Specific Project for a Selected Client</h1>

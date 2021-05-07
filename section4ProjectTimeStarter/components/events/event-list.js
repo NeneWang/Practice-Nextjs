5886bd45f6e7783aecc0be483238973a286339d7
NelsonWang <wangnelson2@gmail.com>
2021-05-07 06:22:47 -0400

    FileBasedProject | Events | Event Components | Event Item | Mapping Data

--- section4ProjectTimeStarter/components/events/event-list.js
+++ section4ProjectTimeStarter/components/events/event-list.js
@@ -1,7 +1,20 @@
 import React from "react";
-import EventItem from './event-item';
+import EventItem from "./event-item";
 
 export default function EventList(props) {
   const { items } = props;
-  return <div>{items.map(event => <EventItem />)}</div>;
+  return (
+    <div>
+      {items.map((event) => (
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          location={event.location}
+          date={event.date}
+          image={event.image}
+        />
+      ))}
+    </div>
+  );
 }

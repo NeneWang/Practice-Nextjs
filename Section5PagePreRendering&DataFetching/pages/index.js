a15df56eddc46916575462aade5369c340105c3e
NelsonWang <wangnelson2@gmail.com>
2021-05-08 08:58:51 -0400

    PreRedendering Project | GetStaticProp | NO Data

--- Section5PagePreRendering&DataFetching/pages/index.js
+++ Section5PagePreRendering&DataFetching/pages/index.js
@@ -1,7 +1,6 @@
-import fs from 'fs/promises';
-import _ from 'lodash';
-import path from 'path';
-
+import fs from "fs/promises";
+import _ from "lodash";
+import path from "path";
 
 function HomePage(props) {
   const { products } = props;
@@ -16,17 +15,29 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
-  console.log('(Re-)Generating...')
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  console.log("(Re-)Generating...");
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
 
   return {
     props: {
       products: data.products,
     },
     revalidate: 10,
+    redirect: 10,
   };
 }
 

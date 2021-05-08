11905f0bbe6da85b6cdb707f3f13c2fb285715f9
NelsonWang <wangnelson2@gmail.com>
2021-05-08 05:39:10 -0400

    FileBasedProject | Layout | Main Headers

--- section4ProjectTimeStarter/components/layout/main-header.js
+++ section4ProjectTimeStarter/components/layout/main-header.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function mainHeader() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <header>
+      <div>
+        <Link href="/">NextEvents</Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/events">All Events</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }

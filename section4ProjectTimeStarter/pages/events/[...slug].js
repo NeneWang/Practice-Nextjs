ac3b32c1a8ae26aa77979bff1a1e503b8462f863
NelsonWang <wangnelson2@gmail.com>
2021-05-08 06:51:56 -0400

    FileBasedProject | Event Search | Events Search Filter

--- section4ProjectTimeStarter/pages/events/[...slug].js
+++ section4ProjectTimeStarter/pages/events/[...slug].js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from '../../components/events/event-list';
 
 export default function FilteredEventsPage() {
-    return (
-        <div>
-            <h1>Filtered Events</h1>
-        </div>
-    )
+  const router = useRouter();
+  const filterData = router.query.slug;
+  //   console.log(filterData);
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numMonth) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please Adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p> No Events found for the chosen filter!</p>;
+  }
+
+  return (
+    <div>
+        <EventList items={filteredEvents} />
+    </div>
+  );
 }

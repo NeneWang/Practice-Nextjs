f88cf27d9f43e37a8328ab909a9c8f1e5893089a
NelsonWang <wangnelson2@gmail.com>
2021-05-08 08:36:24 -0400

    PreRedendering Project | GetStaticProps | index

--- Section5PagePreRendering&DataFetching/pages/index.js
+++ Section5PagePreRendering&DataFetching/pages/index.js
@@ -1,3 +1,8 @@
+import fs from 'fs/promises';
+import _ from 'lodash';
+import path from 'path';
+
+
 function HomePage(props) {
   const { products } = props;
 
@@ -11,6 +16,8 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd());
+  fs.readFileSync()
   return {
     props: {
       products: [{ id: "p1", title: "Product 1" }],

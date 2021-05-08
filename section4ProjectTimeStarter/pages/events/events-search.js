301c632846718f45c962ac6383ae253dd1e0fe56
NelsonWang <wangnelson2@gmail.com>
2021-05-08 06:07:43 -0400

    FileBasedProject | Event Search

---
+++ section4ProjectTimeStarter/pages/events/events-search.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Button from "../ui/button";
+
+export default function EventSearch(props) {
+  return (
+    <form>
+      <div>
+        <label htmlFor="year">Year</label>
+        <select name="" id="year">
+          <option value="2021">2021</option>
+          <option value="2022">2022</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="month">
+          <select name="" id="">
+            <option value="1">January</option>
+            <option value="2">February</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">May</option>
+            <option value="6">June</option>
+            <option value="7">July</option>
+            <option value="8">August</option>
+            <option value="9">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
+          </select>
+        </label>
+      </div>
+      <Button>Find Events</Button>
+    </form>
+  );
+}

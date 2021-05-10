9a9fe1a91853e08d6ee0f827f53cca3b34758e4a
NelsonWang <wangnelson2@gmail.com>
2021-05-10 14:27:41 -0400

    PreRedendering Project | Pages | get Server Side  | Firebase Fetch Fix

--- Section5PagePreRendering&DataFetching/pages/last-sales.js
+++ Section5PagePreRendering&DataFetching/pages/last-sales.js
@@ -1,13 +1,46 @@
+import link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function LastSalesPage() {
-  const [sales, setSales] = useState();
-  const [isLoading, setIsLoad] = useState(false);
+  const [sales, setSales] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch("https://descartable-server-default-rtdb.firebaseio.com/sales.json")
       .then((response) => response.json())
-      .then((data) => {});
+      .then((data) => {
+          console.log(data)
+        const transformSales = [];
+        for (const key in data) {
+          transformSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume,
+          });
+        }
+        console.log("Trasform sales: " );
+        console.log(transformSales)
+        setSales(transformSales);
+        setIsLoading(false);
+      });
   }, []);
-  return <ul></ul>;
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!sales) {
+    return <p>No Data Yet</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
 }

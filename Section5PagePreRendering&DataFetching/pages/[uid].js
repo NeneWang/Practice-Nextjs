2fec2797358b3f31620fcb17280b286dda8f67d8
NelsonWang <wangnelson2@gmail.com>
2021-05-10 13:23:34 -0400

    PreRedendering Project | Pages | uids

---
+++ Section5PagePreRendering&DataFetching/pages/[uid].js
@@ -0,0 +1,17 @@
+function UserIdPage(props) {
+  return <h1>{props.id}</h1>;
+}
+
+export default UserIdPage;
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+
+  const userId = parms.uid;
+
+  return {
+    props: {
+      id: "userid-" + userId,
+    },
+  };
+}
--- Section5PagePreRendering&DataFetching/pages/user-profile.js
+++ Section5PagePreRendering&DataFetching/pages/user-profile.js
@@ -1,9 +1,12 @@
 function UserProfilePage(props) {
-  return <h1>Max</h1>;
+  return <h1>{props.username}</h1>;
 }
 
 export default UserProfilePage;
 
 export async function getServerSideProps(context) {
-  return {};
+  const { params, req, res } = context;
+  //   console.log(req);
+  //   console.log(res);
+  return { props: { username: "Max" } };
 }

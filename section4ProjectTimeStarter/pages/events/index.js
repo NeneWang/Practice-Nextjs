c7b8e06bfbb1fa4cd19d1730c07e509778a9cabe
NelsonWang <wangnelson2@gmail.com>
2021-05-11 21:22:11 -0400

    Project Time : PreRendering | Index Event | Pre-generated

--- section4ProjectTimeStarter/pages/events/index.js
+++ section4ProjectTimeStarter/pages/events/index.js
@@ -1,13 +1,13 @@
 import { Fragment } from "react";
 import { useRouter } from "next/router";
 
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventSearch from "../../components/events/events-search";
 
-export default function AllEventsPage() {
-  const events = getAllEvents();
+export default function AllEventsPage(props) {
   const router = useRouter();
+  const { events } = props;
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}/`;
@@ -22,4 +22,12 @@ export default function AllEventsPage() {
   );
 }
 
-
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60
+  };
+}

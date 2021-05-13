48f30ab81a3eef742cfe3b33c2dc1bd49866d303
NelsonWang <wangnelson2@gmail.com>
2021-05-12 23:52:14 -0400

    Dynamic event meta

--- Section7OptimizingNextJSApps/pages/events/[eventId].js
+++ Section7OptimizingNextJSApps/pages/events/[eventId].js
@@ -1,10 +1,11 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import Head from 'next/head';
 
-import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
-import EventSummary from '../../components/event-detail/event-summary';
-import EventLogistics from '../../components/event-detail/event-logistics';
-import EventContent from '../../components/event-detail/event-content';
-import ErrorAlert from '../../components/ui/error-alert';
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
 
 function EventDetailPage(props) {
   const event = props.selectedEvent;
@@ -19,6 +20,13 @@ function EventDetailPage(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>{event.title}</title>
+        <meta
+          name="description"
+          content={event.description}
+        />
+      </Head>
       <EventSummary title={event.title} />
       <EventLogistics
         date={event.date}
@@ -40,20 +48,20 @@ export async function getStaticProps(context) {
 
   return {
     props: {
-      selectedEvent: event
+      selectedEvent: event,
     },
-    revalidate: 30
+    revalidate: 30,
   };
 }
 
 export async function getStaticPaths() {
   const events = await getFeaturedEvents();
 
-  const paths = events.map(event => ({ params: { eventId: event.id } }));
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
 
   return {
     paths: paths,
-    fallback: 'blocking'
+    fallback: "blocking",
   };
 }
 

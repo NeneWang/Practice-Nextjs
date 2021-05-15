d0ee17f336a01eb49baba87d529d7c5a366951d0
NelsonWang <wangnelson2@gmail.com>
2021-05-14 20:00:00 -0600

    viewpoert | device width

--- Section7OptimizingNextJSApps/pages/_app.js
+++ Section7OptimizingNextJSApps/pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <meta name="viewport" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"  />
       </Head>
       <Component {...pageProps} />
     </Layout>

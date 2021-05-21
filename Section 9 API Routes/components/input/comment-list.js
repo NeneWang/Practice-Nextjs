f91652bf6757a8f39f7c93dde303f75b054684a6
NelsonWang <wangnelson2@gmail.com>
2021-05-21 18:34:22 -0400

    P9 API | registration | comments | Fetched

--- Section 9 API Routes/components/input/comment-list.js
+++ Section 9 API Routes/components/input/comment-list.js
@@ -5,18 +5,14 @@ function CommentList(props) {
   return (
     <ul className={classes.comments}>
       {/* Render list of comments - fetched from API */}
-      <li>
-        <p>My comment is amazing!</p>
-        <div>
-          By <address>Maximilian</address>
-        </div>
-      </li>
-      <li>
-        <p>My comment is amazing!</p>
-        <div>
-          By <address>Maximilian</address>
-        </div>
-      </li>
+      {items.map((item) => (
+        <li ke={item.id}>
+          <p>{item.text}</p>
+          <div>
+            By <address>{item.name}</address>
+          </div>
+        </li>
+      ))}
     </ul>
   );
 }

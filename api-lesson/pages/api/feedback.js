0dc63ab8f2f64d98cb85a9e2ff8be90e5068c8c7
NelsonWang <wangnelson2@gmail.com>
2021-05-21 13:42:21 -0400

    Api | Feedback

--- api-lesson/pages/api/feedback.js
+++ api-lesson/pages/api/feedback.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import fs from "fs";
+import path from "path";
 
 export default function handler(req, res) {
-    if(req.method === 'POST'){
-        
-    }
-    res.status(200).json({message: 'This Works!'});
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedbackText,
+    };
+
+    //Store that in a database or in a file
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+
+}
+  res.status(200).json({ message: "This Works!" });
 }

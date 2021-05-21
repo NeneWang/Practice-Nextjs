408e59589270277ea716bfa91534e421425ffe25
NelsonWang <wangnelson2@gmail.com>
2021-05-21 18:19:48 -0400

    P9 API | registration | new comments

--- Section 9 API Routes/pages/api/comments/[eventId].js
+++ Section 9 API Routes/pages/api/comments/[eventId].js
@@ -1,49 +1,64 @@
-function handler(req, res) {
-  const eventId = req.query.eventId;
-
-  if (req.method === "POST") {
-    const { email, name, text } = req.body;
-
-    if (
-      !email.includes("@") ||
-      !name ||
-      name.trim() === "" ||
-      !text ||
-      text.trim() === ""
-    ) {
-      res.status(422).json({ message: "Invalid input." });
+import {
+    connectDatabase,
+    insertDocument,
+    getAllDocuments,
+  } from '../../../helpers/db-util';
+  
+  async function handler(req, res) {
+    const eventId = req.query.eventId;
+  
+    let client;
+  
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: 'Connecting to the database failed!' });
       return;
     }
-
-    console.log(email, name, text);
-    const newComment = {
-      id: new Date().isISOString(),
-      email,
-      name,
-      text,
-    };
-    console.log(newComment);
-    res.status(201).json({ message: "Added comment.", comment: newComment });
+  
+    if (req.method === 'POST') {
+      const { email, name, text } = req.body;
+  
+      if (
+        !email.includes('@') ||
+        !name ||
+        name.trim() === '' ||
+        !text ||
+        text.trim() === ''
+      ) {
+        res.status(422).json({ message: 'Invalid input.' });
+        client.close();
+        return;
+      }
+  
+      const newComment = {
+        email,
+        name,
+        text,
+        eventId,
+      };
+  
+      let result;
+  
+      try {
+        result = await insertDocument(client, 'comments', newComment);
+        newComment._id = result.insertedId;
+        res.status(201).json({ message: 'Added comment.', comment: newComment });
+      } catch (error) {
+        res.status(500).json({ message: 'Inserting comment failed!' });
+      }
+    }
+  
+    if (req.method === 'GET') {
+      try {
+        const documents = await getAllDocuments(client, 'comments', { _id: -1 });
+        res.status(200).json({ comments: documents });
+      } catch (error) {
+        res.status(500).json({ message: 'Getting comments failed.' });
+      }
+    }
+  
+    client.close();
   }
-
-  if (req.method === "GET") {
-    const dummyList = [
-      {
-        id: "c1",
-        name: "Max",
-        email: "test@test.com",
-        comments: "A first comment!",
-      },
-      {
-        id: "c2",
-        name: "Manuel",
-        email: "test@test.com",
-        comments: "A second comment!",
-      },
-    ];
-
-    res.status(200).json({ comments: dummyList });
-  }
-}
-
-export default handler;
+  
+  export default handler;

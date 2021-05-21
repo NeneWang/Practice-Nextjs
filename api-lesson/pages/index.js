01804e5c115e8e876d691c2be023ff0e18ba547e
NelsonWang <wangnelson2@gmail.com>
2021-05-21 13:24:14 -0400

    Api | Routes?

--- api-lesson/pages/index.js
+++ api-lesson/pages/index.js
@@ -2,6 +2,17 @@ function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
+      <form action="">
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" name="" id="email" />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback</label>
+          <textarea rows="5" id="feedback"></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }

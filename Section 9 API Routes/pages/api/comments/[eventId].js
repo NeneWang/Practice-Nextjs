cf11c6df31ee043329560abdce83f204982fd5cc
NelsonWang <wangnelson2@gmail.com>
2021-05-21 18:27:11 -0400

    P9 API | registration | event-id

--- Section 9 API Routes/pages/api/comments/[eventId].js
+++ Section 9 API Routes/pages/api/comments/[eventId].js
@@ -1,20 +1,5 @@
-import {
-    connectDatabase,
-    insertDocument,
-    getAllDocuments,
-  } from '../../../helpers/db-util';
-  
-  async function handler(req, res) {
+function handler(req, res) {
     const eventId = req.query.eventId;
-  
-    let client;
-  
-    try {
-      client = await connectDatabase();
-    } catch (error) {
-      res.status(500).json({ message: 'Connecting to the database failed!' });
-      return;
-    }
   
     if (req.method === 'POST') {
       const { email, name, text } = req.body;
@@ -27,38 +12,29 @@ import {
         text.trim() === ''
       ) {
         res.status(422).json({ message: 'Invalid input.' });
-        client.close();
         return;
       }
   
       const newComment = {
+        id: new Date().toISOString(),
         email,
         name,
         text,
-        eventId,
       };
   
-      let result;
+      console.log(newComment);
   
-      try {
-        result = await insertDocument(client, 'comments', newComment);
-        newComment._id = result.insertedId;
-        res.status(201).json({ message: 'Added comment.', comment: newComment });
-      } catch (error) {
-        res.status(500).json({ message: 'Inserting comment failed!' });
-      }
+      res.status(201).json({ message: 'Added comment.', comment: newComment });
     }
   
     if (req.method === 'GET') {
-      try {
-        const documents = await getAllDocuments(client, 'comments', { _id: -1 });
-        res.status(200).json({ comments: documents });
-      } catch (error) {
-        res.status(500).json({ message: 'Getting comments failed.' });
-      }
-    }
+      const dummyList = [
+        { id: 'c1', name: 'Max', text: 'A first comment!' },
+        { id: 'c2', name: 'Manuel', text: 'A second comment!' },
+      ];
   
-    client.close();
+      res.status(200).json({ comments: dummyList });
+    }
   }
   
   export default handler;

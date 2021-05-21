f056533775b5b0d5eec27cb830df156b61186b60
NelsonWang <wangnelson2@gmail.com>
2021-05-21 13:26:53 -0400

    Api | Routes | Form | Submission

--- api-lesson/pages/index.js
+++ api-lesson/pages/index.js
@@ -1,15 +1,24 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
-      <form action="">
+      <form action="" onSubmit={submitFormHandler}>
         <div>
           <label htmlFor="email">Your Email Address</label>
-          <input type="email" name="" id="email" />
+          <input type="email" name="" id="email" ref={emailInputRef} />
         </div>
         <div>
           <label htmlFor="feedback">Your feedback</label>
-          <textarea rows="5" id="feedback"></textarea>
+          <textarea rows="5" id="feedback" ref={feedbackInputRef}></textarea>
         </div>
         <button>Send Feedback</button>
       </form>

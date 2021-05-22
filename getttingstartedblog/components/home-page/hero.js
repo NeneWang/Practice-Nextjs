a9a698dc1a8e40089e96a8769368527554234774
NelsonWang <wangnelson2@gmail.com>
2021-05-22 09:23:17 -0400

    Section 11: Full Blog A - Z | Base Pages |  Reset

---
+++ getttingstartedblog/components/home-page/hero.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+import React from "react";
+
+export default function hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/max.png"
+          alt="An image showing Max"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I am max</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like
+        Angular or React
+      </p>
+    </section>
+  );
+}
---
+++ getttingstartedblog/pages/index.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+import Hero from '../components/home-page/hero'
+
+export default function index() {
+  return (
+    <Fragment>
+    <Hero />
+  </Fragment>
+  )
+}

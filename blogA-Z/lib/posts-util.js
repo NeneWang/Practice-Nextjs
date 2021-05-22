c4778caa38a71617c02e50bd4e5073d4c65501a5
NelsonWang <wangnelson2@gmail.com>
2021-05-22 13:39:32 -0400

    Section 11: Full Blog A - Z | Base Pages | markdown | fix post

--- blogA-Z/lib/posts-util.js
+++ blogA-Z/lib/posts-util.js
@@ -1,42 +1,45 @@
-import fs from "fs";
-import path from "path";
-import React from "react";
+import fs from 'fs';
+import path from 'path';
 
-import matter from "gray-matter";
+import matter from 'gray-matter';
 
-const postsDirectory = path.join(process.cwd(), "posts");
+const postsDirectory = path.join(process.cwd(), 'posts');
 
-function getPostData(fileName) {
-  const filePath = path.join(postsDirectory, fileName);
-  const fileContent = fs.readFieSync(filePath, "utf-8");
+export function getPostsFiles() {
+  return fs.readdirSync(postsDirectory);
+}
+
+export function getPostData(postIdentifier) {
+  const postSlug = postIdentifier.replace(/\.md$/, ''); // removes the file extension
+  const filePath = path.join(postsDirectory, `${postSlug}.md`);
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
   const { data, content } = matter(fileContent);
 
-  const postSlug = filename.replace(/\.md$/, ""); //Removes the file extension
   const postData = {
     slug: postSlug,
     ...data,
     content,
   };
+
+  return postData;
 }
 
 export function getAllPosts() {
-  const postFiles = fs.readdyrSync(postsDirectory);
+  const postFiles = getPostsFiles();
 
-  const allPosts = postFiles.map((postFile) => {
+  const allPosts = postFiles.map(postFile => {
     return getPostData(postFile);
   });
 
-  const sortedPosts = allPosts.sort((postA, postB) =>
-    postA.date > postB.date ? -1 : 1
-  );
+  const sortedPosts = allPosts.sort((postA, postB) => postA.date > postB.date ? -1 : 1);
 
   return sortedPosts;
 }
 
 export function getFeaturedPosts() {
-  const allPosts = getAllPosts();
-
-  const featuredPosts = allPosts.filter((post) => post.isFeatured);
-
-  return featuredPosts;
-}
+    const allPosts = getAllPosts();
+  
+    const featuredPosts = allPosts.filter(post => post.isFeatured);
+  
+    return featuredPosts;
+  }
--- blogA-Z/pages/index.js
+++ blogA-Z/pages/index.js
@@ -3,46 +3,25 @@ import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Stated with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production, it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
-    date: '2022-02-10' ,
-  },
-  {
-    slug: "getting-started-with-nextjs2",
-    title: "Getting Stated with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production, it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
-    date: '2022-02-10' ,
-  },
-  {
-    slug: "getting-started-with-nextjs3",
-    title: "Getting Stated with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production, it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
-    date: '2022-02-10' ,
-  },
-  {
-    slug: "getting-started-with-nextjs4",
-    title: "Getting Stated with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production, it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
-    date: '2022-02-10' ,
-  },
-];
+import { getFeaturedPosts } from "../lib/posts-util";
 
-export default function index() {
+
+export default function HomePage(props) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts  posts={DUMMY_POSTS} />
+      <FeaturedPosts  posts={props.posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps(){
+  const featuredPosts = getFeaturedPosts();
+
+  return{
+    props: {
+      posts: featuredPosts
+    },
+    revalidate: 60
+  }
 }

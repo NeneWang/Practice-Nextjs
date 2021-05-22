ae7715490d7de82538e9df65ad6de048e955c2ba
NelsonWang <wangnelson2@gmail.com>
2021-05-22 12:39:28 -0400

    Section 11: Full Blog A - Z | Base Pages | post content + Header | this is the post content | styling

--- blogA-Z/pages/posts/[slug].js
+++ blogA-Z/pages/posts/[slug].js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import PostContent from './post-detail/post-content'
 
+
 export default function PostDetailPage() {
     return (
         <PostContent />
--- blogA-Z/pages/posts/post-detail/post-content.js
+++ blogA-Z/pages/posts/post-detail/post-content.js
@@ -1,4 +1,5 @@
 import PostHeader from './post-header'
+import classes from './post-content.module.css';
 
 const DUMMY_POSTS = {
   slug: "getting-started-with-nextjs",
@@ -14,7 +15,7 @@ import React from "react";
 export default function PostContent() {
   const imagePath = `/images/posts/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`;
   return (
-    <article>
+    <article className={classes.content} >
       <PostHeader title={DUMMY_POSTS.title} image={imagePath} />
       {DUMMY_POSTS.content}
     </article>

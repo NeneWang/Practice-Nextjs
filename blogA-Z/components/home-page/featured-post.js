d281fbfe01a0c2e5f8cc1e5c13a96a574b882953
NelsonWang <wangnelson2@gmail.com>
2021-05-22 11:41:53 -0400

    Section 11: Full Blog A - Z | Base Pages | rendering dummy data

--- blogA-Z/components/home-page/featured-post.js
+++ blogA-Z/components/home-page/featured-post.js
@@ -1,9 +1,11 @@
+import PostGrid from "../posts/posts-grid";
 import classes from "./featured-posts.module.css";
 
-function FeaturedPosts() {
+function FeaturedPosts(props) {
   return (
-    <section className={classes.latest} >
+    <section className={classes.latest}>
       <h2>Featured Posts</h2>
+      <PostGrid key={post.slug} post={post} />
     </section>
   );
 }

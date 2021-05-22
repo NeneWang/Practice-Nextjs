4468718206e53713b55f9077b1593aac8f30e3b7
NelsonWang <wangnelson2@gmail.com>
2021-05-21 20:50:07 -0400

    P9 API | Context | Notification components | added

--- Section 9 API Routes/pages/_app.js
+++ Section 9 API Routes/pages/_app.js
@@ -1,17 +1,20 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Layout from "../components/layout/layout";
+
+import Notification from "../components/ui/notification";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
         <title>Next Events</title>
-        <meta name='description' content='NextJS Events' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name="description" content="NextJS Events" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification title="Test" message="This is a test" status="pending" />
     </Layout>
   );
 }

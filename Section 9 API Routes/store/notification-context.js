819b7881c0ab271ceeda8c90837e026cb5f93a6e
NelsonWang <wangnelson2@gmail.com>
2021-05-21 21:32:01 -0400

    P9 API | Context | Notification components | context fixed

--- Section 9 API Routes/store/notification-context.js
+++ Section 9 API Routes/store/notification-context.js
@@ -1,13 +1,29 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from 'react';
 
 const NotificationContext = createContext({
-  Notification: null,
-  showNotification: function (notificationdata) {},
+  notification: null, // { title, message, status }
+  showNotification: function (notificationData) {},
   hideNotification: function () {},
 });
 
 export function NotificationContextProvider(props) {
   const [activeNotification, setActiveNotification] = useState();
+
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === 'success' ||
+        activeNotification.status === 'error')
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
 
   function showNotificationHandler(notificationData) {
     setActiveNotification(notificationData);
@@ -30,4 +46,4 @@ export function NotificationContextProvider(props) {
   );
 }
 
-export default notificationContext;
+export default NotificationContext;

49914aeb16aa5458fccd427f33798dc8d37527ea
NelsonWang <wangnelson2@gmail.com>
2021-05-21 21:17:51 -0400

    P9 API | Context | Notification components | active notification rendering

--- Section 9 API Routes/components/layout/layout.js
+++ Section 9 API Routes/components/layout/layout.js
@@ -2,17 +2,23 @@ import { Fragment, useContext } from "react";
 
 import MainHeader from "./main-header";
 import Notification from "../ui/notification";
-
+import NotificationContext from "../../store/notification-context";
 
 function Layout(props) {
-  useContext();
+  const notificationCtx = useContext(NotificationContext);
+  const activeNotification = notificationCtx.notification;
 
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
-
-      <Notification title="Test" message="This is a test" status="pending" />
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </Fragment>
   );
 }

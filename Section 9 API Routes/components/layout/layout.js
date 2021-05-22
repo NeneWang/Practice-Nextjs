9cbb1a5ab14895a7ed3b8e4518c2d71ee267500f
NelsonWang <wangnelson2@gmail.com>
2021-05-21 21:12:02 -0400

    P9 API | Context | Notification components | layout implementation

--- Section 9 API Routes/components/layout/layout.js
+++ Section 9 API Routes/components/layout/layout.js
@@ -1,12 +1,18 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from "react";
 
-import MainHeader from './main-header';
+import MainHeader from "./main-header";
+import Notification from "../ui/notification";
+
 
 function Layout(props) {
+  useContext();
+
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+
+      <Notification title="Test" message="This is a test" status="pending" />
     </Fragment>
   );
 }

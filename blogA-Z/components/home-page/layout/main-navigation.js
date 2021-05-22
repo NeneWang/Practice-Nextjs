3506a53b7a8597f6faa7f347ff7cdf5152d91d2b
NelsonWang <wangnelson2@gmail.com>
2021-05-22 09:34:24 -0400

    Section 11: Full Blog A - Z | Base Pages | Logo

--- blogA-Z/components/home-page/layout/main-navigation.js
+++ blogA-Z/components/home-page/layout/main-navigation.js
@@ -4,11 +4,16 @@ import Link from "next/link";
 export default function MainNavigation() {
   return (
     <header>
-      <Logo />
+      <Link href="/">
+        <Logo />
+      </Link>
       <nav>
         <ul>
           <li>
             <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
           </li>
         </ul>
       </nav>

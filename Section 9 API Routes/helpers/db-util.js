5335924f4f61892bed90e209889c9dec4ef6a9bf
NelsonWang <wangnelson2@gmail.com>
2021-05-21 20:40:19 -0400

    P9 API | mongodb database | get all comments

--- Section 9 API Routes/helpers/db-util.js
+++ Section 9 API Routes/helpers/db-util.js
@@ -14,7 +14,7 @@ export async function insertDocument(client, collection, document) {
   const db = client.db();
 
   const result = await db.collection(collection).insertOne(document);
-    
+
   return result;
 }
 
--- Section 9 API Routes/pages/api/newsletter.js
+++ Section 9 API Routes/pages/api/newsletter.js
@@ -1,33 +1,46 @@
-import { connectDatabase, insertDocument } from '../../helpers/db-util';
+import { MongoClient } from "mongodb";
 
-async function handler(req, res) {
-  if (req.method === 'POST') {
-    const userEmail = req.body.email;
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://nelson:1223@cluster0.kzhr5.mongodb.net/events?retryWrites=true&w=majority"
+  );
 
-    if (!userEmail || !userEmail.includes('@')) {
-      res.status(422).json({ message: 'Invalid email address.' });
-      return;
-    }
-
-    let client;
-
-    try {
-      client = await connectDatabase();
-    } catch (error) {
-      res.status(500).json({ message: 'Connecting to the database failed!' });
-      return;
-    }
-
-    try {
-      await insertDocument(client, 'newsletter', { email: userEmail });
-      client.close();
-    } catch (error) {
-      res.status(500).json({ message: 'Inserting data failed!' });
-      return;
-    }
-
-    res.status(201).json({ message: 'Signed up!' });
-  }
+  return client;
 }
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection("newsletter").insertOne({ email: userEmail });
+}
+async function handler(req, res) {
+    if (req.method === 'POST') {
+      const userEmail = req.body.email;
+  
+      if (!userEmail || !userEmail.includes('@')) {
+        res.status(422).json({ message: 'Invalid email address.' });
+        return;
+      }
+  
+      let client;
+  
+      try {
+        client = await connectDatabase();
+      } catch (error) {
+        res.status(500).json({ message: 'Connecting to the database failed!' });
+        return;
+      }
+  
+      try {
+        await insertDocument(client, 'newsletter', { email: userEmail });
+        client.close();
+      } catch (error) {
+        res.status(500).json({ message: 'Inserting data failed!' });
+        return;
+      }
+  
+      res.status(201).json({ message: 'Signed up!' });
+    }
+  }
+  
 
 export default handler;

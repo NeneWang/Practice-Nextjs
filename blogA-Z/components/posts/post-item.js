4f6af2255e211e724076c414fbc0c76b1c7085f0
NelsonWang <wangnelson2@gmail.com>
2021-05-22 11:28:43 -0400

    Section 11: Full Blog A - Z | Base Pages | postgrid

--- blogA-Z/components/posts/post-item.js
+++ blogA-Z/components/posts/post-item.js
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from 'next/image';
+
 import classes from "./posts-item.module.css";
 
 function PostItem(props) {
@@ -10,14 +12,14 @@ function PostItem(props) {
       year: 'numeric'
   });
 
-  const imagePath = '/images/posts';
+  const imagePath = `/images/posts/${slug}/${image}`;
 
   return (
     <li className={classes.post}>
       <Link>
         <a href="">
           <div className={classes.image}>
-            <Image src="" alt={title} width={3000} height={200} />
+            <Image src={imagePath} alt={title} width={3000} height={200} />
           </div>
           <div className={classes.content}>
             <h3>{title}</h3>

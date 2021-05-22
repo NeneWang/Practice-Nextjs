2e494bb9d52c9bc60d221d49d615c6f64bf321f3
NelsonWang <wangnelson2@gmail.com>
2021-05-21 21:40:00 -0400

    P9 API | Context | Notification components | hidenotification onClick

--- Section 9 API Routes/components/ui/notification.js
+++ Section 9 API Routes/components/ui/notification.js
@@ -4,6 +4,8 @@ import classes from './notification.module.css';
 import NotificationContext from '../../store/notification-context';
 
 function Notification(props) {
+
+
   const notificationCtx = useContext(NotificationContext);
 
   const { title, message, status } = props;

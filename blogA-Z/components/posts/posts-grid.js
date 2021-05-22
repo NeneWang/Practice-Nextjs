69bae75f71f0b3a0c0d0e0ae86f8c48f90100d95
NelsonWang <wangnelson2@gmail.com>
2021-05-22 11:38:29 -0400

    Section 11: Full Blog A - Z | Base Pages | featured posts

--- blogA-Z/components/posts/posts-grid.js
+++ blogA-Z/components/posts/posts-grid.js
@@ -4,9 +4,9 @@ import classes from './posts-grid.module.css'
 function PostGrid(props) {
   const { posts } = props;
   return (
-    <ul className={classes.grid} >
+    <ul className={classes.grid}>
       {posts.map((post) => (
-        <PostItem />
+        <PostItem key={post.slug} post={post} />
       ))}
     </ul>
   );

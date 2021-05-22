39e3a0bb15c53960098b79c526007a539696cbbc
NelsonWang <wangnelson2@gmail.com>
2021-05-22 09:07:38 -0400

    Section 11: Full Blog A - Z | Base Pages | Hero Srcs

--- blogA-Z/components/home-page/hero.js
+++ blogA-Z/components/home-page/hero.js
@@ -1,12 +1,20 @@
+import Image from "next/image";
 import classes from "./hero.module.css";
 import React from "react";
 
 export default function hero() {
   return (
-    <section className= {classess.hero}>
-      <div className={classess.image}>Image</div>
+    <section className={classess.hero}>
+      <div className={classess.image}>
+        <Image
+          src="/images/site/max.png"
+          alt="An image showing Max"
+          width={300}
+          height={300}
+        />
+      </div>
       <h1>Hi, I am max</h1>
-      <p  >
+      <p>
         I blog about web development - especially frontend frameworks like
         Angular or React
       </p>

7bc4be0e302673b82f21a1e51f5259e227734303
NelsonWang <wangnelson2@gmail.com>
2021-05-22 12:31:30 -0400

    Section 11: Full Blog A - Z | Base Pages | post content + Header | dummy content

--- blogA-Z/pages/posts/post-detail/post-content.js
+++ blogA-Z/pages/posts/post-detail/post-content.js
@@ -1,5 +1,10 @@
 const DUMMY_POSTS = {
-  slug: "",
+  slug: "getting-started-with-nextjs",
+  title: "Getting Stated with NextJS",
+  image: "getting-started-nextjs.png",
+  excerpt:
+    "NextJS is the React framework for production, it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
+  date: "2022-02-10",
 };
 import React from "react";
 

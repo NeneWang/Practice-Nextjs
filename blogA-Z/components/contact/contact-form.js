ab843fd9bb0d546db698408fe1ba77325c40aacc
NelsonWang <wangnelson2@gmail.com>
2021-05-23 11:32:19 -0400

    before duplicating instructors

--- blogA-Z/components/contact/contact-form.js
+++ blogA-Z/components/contact/contact-form.js
@@ -1,83 +1,111 @@
-import { useState } from "react";
-import classes from "./contact-form.module.css";
-import Notification from "../ui/notification";
+import { useState, useEffect } from 'react';
+
+import classes from './contact-form.module.css';
+import Notification from '../ui/notification';
 
 async function sendContactData(contactDetails) {
-  const reponse = await fetch("/api/contact", {
-    method: "POST",
-    body: JSON.stringify({
-      email: enteredEmail,
-      name: enteredName,
-      message: enteredMessage,
-    }),
+  const response = await fetch('/api/contact', {
+    method: 'POST',
+    body: JSON.stringify(contactDetails),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Something went wrong!');
+  }
 }
 
-async function ContactForm() {
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [enteredName, setEnteredName] = useState("");
-  const [enteredMessage, setEnteredMessage] = useState("");
-  const [requestStatus, setRequestStaus] = useState();
+function ContactForm() {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredMessage, setEnteredMessage] = useState('');
+  const [requestStatus, setRequestStatus] = useState(); // 'pending', 'success', 'error'
+  const [requestError, setRequestError] = useState();
 
-  async function sendContactData(contactDetails) {
-    const reponse = await fetch("/api/contact", {
-      method: "POST",
-      body: JSON.stringify(contactDetails),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  useEffect(() => {
+    if (requestStatus === 'success' || requestStatus === 'error') {
+      const timer = setTimeout(() => {
+        setRequestStatus(null);
+        setRequestError(null);
+      }, 3000);
 
-    const data = await reponse.json();
-    if (!reponse.ok) {
-      throw new Error(data.message || "Something went wrong");
+      return () => clearTimeout(timer);
     }
-  }
+  }, [requestStatus]);
 
   async function sendMessageHandler(event) {
     event.preventDefault();
 
-    setRequestStatus("pending");
+    // optional: add client-side validation
+
+    setRequestStatus('pending');
 
     try {
       await sendContactData({
         email: enteredEmail,
         name: enteredName,
         message: enteredMessage,
-        
       });
-      setRequestStaus('success');
+      setRequestStatus('success');
+      setEnteredMessage('');
+      setEnteredEmail('');
+      setEnteredName('');
     } catch (error) {
-      setRequestStaus("error");
+      setRequestError(error.message);
+      setRequestStatus('error');
     }
+  }
 
-    setRequestStaus("success");
+  let notification;
+
+  if (requestStatus === 'pending') {
+    notification = {
+      status: 'pending',
+      title: 'Sending message...',
+      message: 'Your message is on its way!',
+    };
+  }
+
+  if (requestStatus === 'success') {
+    notification = {
+      status: 'success',
+      title: 'Success!',
+      message: 'Message sent successfully!',
+    };
+  }
+
+  if (requestStatus === 'error') {
+    notification = {
+      status: 'error',
+      title: 'Error!',
+      message: requestError,
+    };
   }
 
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form} action="" onSubmit={sendMessageHandler}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
-            <label htmlFor="email">Your Email</label>
+            <label htmlFor='email'>Your Email</label>
             <input
-              type="email"
-              id="email"
+              type='email'
+              id='email'
               required
               value={enteredEmail}
               onChange={(event) => setEnteredEmail(event.target.value)}
             />
           </div>
-
           <div className={classes.control}>
-            <label htmlFor="name">Your Name</label>
+            <label htmlFor='name'>Your Name</label>
             <input
-              type="text"
-              id="name"
+              type='text'
+              id='name'
               required
               value={enteredName}
               onChange={(event) => setEnteredName(event.target.value)}
@@ -85,20 +113,27 @@ async function ContactForm() {
           </div>
         </div>
         <div className={classes.control}>
-          <label htmlFor="message">Your message</label>
+          <label htmlFor='message'>Your Message</label>
           <textarea
-            id="message"
-            rows="5"
+            id='message'
+            rows='5'
             required
             value={enteredMessage}
             onChange={(event) => setEnteredMessage(event.target.value)}
           ></textarea>
         </div>
 
-        <div clasName={classes.action}>
+        <div className={classes.actions}>
           <button>Send Message</button>
         </div>
       </form>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
     </section>
   );
 }

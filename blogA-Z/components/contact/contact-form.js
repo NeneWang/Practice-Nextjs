e294fbffb29666f980850a4ece0f1a4468da88fc
NelsonWang <wangnelson2@gmail.com>
2021-05-22 22:27:14 -0400

    Section 11: Full Blog A - Z | Base Pages | slug | post header | api contact form

--- blogA-Z/components/contact/contact-form.js
+++ blogA-Z/components/contact/contact-form.js
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import classes from "./contact-form.module.css";
+
 function ContactForm() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+
+  function sendMessageHandler(event) {
+    event.preventDefault();
+
+    fetch("/api/contact", {
+      method: "POST",
+    });
+  }
+
   return (
-    <section className={classes.contact} >
+    <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form} action="">
+      <form className={classes.form} action="" onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              id="email"
+              required
+              value={enteredEmail}
+              onChange={(event) => setEnteredEmail(event.target.value)}
+            />
           </div>
 
           <div className={classes.control}>

ef0812c11ed8ed71401e3c9cc961dd50d1e12f7a
NelsonWang <wangnelson2@gmail.com>
2021-05-23 11:37:58 -0400

    blog az- intructors code fixed

--- blogazinstructor/nextjs-course-code-10-prj-blog/pages/api/contact.js
+++ blogazinstructor/nextjs-course-code-10-prj-blog/pages/api/contact.js
@@ -25,9 +25,7 @@ async function handler(req, res) {
     let client;
 
     try {
-      client = await MongoClient.connect(
-        'mongodb+srv://maximilian:2YkcXq43KyPk0vqp@cluster0.ntrwp.mongodb.net/my-site?retryWrites=true&w=majority'
-      );
+      client = await MongoClient.connect('mongodb+srv://nelson:1223@cluster0.kzhr5.mongodb.net/my-site?retryWrites=true&w=majority');
     } catch (error) {
       res.status(500).json({ message: 'Could not connect to database.' });
       return;

a0baf041b5ef9fce7a5bf8ecdb39ab49478fdf60
NelsonWang <wangnelson2@gmail.com>
2021-05-22 22:53:45 -0400

    Section 11: Full Blog A - Z | Base Pages | slug | post header | contact | MONGO DB | context

--- blogA-Z/pages/api/contact.js
+++ blogA-Z/pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -18,7 +20,29 @@ function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://nelson:1223@cluster0.kzhr5.mongodb.net/my-site?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database" });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed !" });
+      return;
+    }
+
+    client.close();
 
     res
       .status(201)

f7705aedf1735c843ea04290fc4a540f5ab56ea7
NelsonWang <wangnelson2@gmail.com>
2021-05-24 23:52:41 -0400

    build?

--- blogA-Z/pages/api/contact.js
+++ blogA-Z/pages/api/contact.js
@@ -24,8 +24,10 @@ async function handler(req, res) {
 
     let client;
 
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.ntrwp.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+
     try {
-      client = await MongoClient.connect('mongodb+srv://nelson:1223@cluster0.kzhr5.mongodb.net/my-site?retryWrites=true&w=majority');
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
       res.status(500).json({ message: 'Could not connect to database.' });
       return;

c3f9fced26c358bd02afead299de3bf534d25ef2
NelsonWang <wangnelson2@gmail.com>
2021-05-25 23:22:13 -0400

    section13 auth | install | lib db | res error

--- sectionAddingAuthentication/pages/api/auth/signup.js
+++ sectionAddingAuthentication/pages/api/auth/signup.js
@@ -3,7 +3,26 @@ import React from "react";
 import { connectToDatabase } from "../../../lib/db";
 
 export default async function handler(req, res) {
+  const req = req.body;
+
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes("@" || !password || password.trim().length < 7)
+  ) {
+    res
+      .status(422)
+      .json({
+        message:
+          "Invalid input - password should also be least 7 character long.",
+      });
+    return;
+  }
   const client = await connectToDatabase();
   const db = client.db();
-  db.collection("users");
+  db.collection("users").insertOne({
+    email: email,
+    password: password,
+  });
 }

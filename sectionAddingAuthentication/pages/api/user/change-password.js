6f0e8ebcedacec1a738329f63a3c0ae1b52224f8
NelsonWang <wangnelson2@gmail.com>
2021-05-26 16:06:13 -0400

    section13 auth | install | lib db | change-passwords

--- sectionAddingAuthentication/pages/api/user/change-password.js
+++ sectionAddingAuthentication/pages/api/user/change-password.js
@@ -1,4 +1,6 @@
 import { getSession } from "next-auth/client";
+
+import { hashPassword, verifyPassword } from "../../../lib/auth";
 import { connectToDatabase } from "../../../lib/db";
 
 async function handler(req, res) {
@@ -18,10 +20,33 @@ async function handler(req, res) {
   const newPassword = req.body.newPassword;
 
   const client = await connectToDatabase();
-  const usersCollection = client.db().collection('users');
+  const usersCollection = client.db().collection("users");
 
-  usersCollection.findOne({email: userEmail});
+  const user = await usersCollection.findOne({ email: userEmail });
 
+  if (!user) {
+    res.status(404).json({ message: "User not found" });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password;
+  const passwordAreEqual = await verifyPassword(oldPassword, currentPassword);
+  if (!passwordAreEqual) {
+    res.status(403).json({ message: "Invalid Password." });
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(newPassword);
+
+  const result = await usersCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: newPassword } }
+  );
+
+  client.close();
+  res.status(200).json({ message: "Password updated!" });
 }
 
 export default handler;

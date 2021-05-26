b09702060a78eb3883d7cdb315494ee4e15a5410
NelsonWang <wangnelson2@gmail.com>
2021-05-25 23:11:33 -0400

    section13 auth | install | lib db | connect mongodb

--- sectionAddingAuthentication/lib/db.js
+++ sectionAddingAuthentication/lib/db.js
@@ -1,5 +1,7 @@
 import { MongoClient } from "mongodb";
 
 function connectToDatabase(){
-    MongoClient.connectToDatabase();
+    const client = await MongoClient.connect('mongodb+srv://nelson: @cluster0.kzhr5.mongodb.net/auth-demo?retryWrites=true&w=majority');
+
+    return client;
 }

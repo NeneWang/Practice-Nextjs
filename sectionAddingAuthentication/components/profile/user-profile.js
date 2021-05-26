57c4442ec86aeb3109d178b39c9b02b4a1839fc2
NelsonWang <wangnelson2@gmail.com>
2021-05-26 15:18:22 -0400

    section13 auth | install | lib db | is loading

--- sectionAddingAuthentication/components/profile/user-profile.js
+++ sectionAddingAuthentication/components/profile/user-profile.js
@@ -1,14 +1,29 @@
-import { useSession } from "next-auth/client";
+import { useSession, getSession } from "next-auth/client";
 
 import ProfileForm from "./profile-form";
 import classes from "./user-profile.module.css";
+import { useEffect, useState } from "react";
 
 function UserProfile() {
   // Redirect away if NOT auth
+  const [isLoading, setIsLoading] = useState(true);
+  // const [loadedSession, setLoadedSession] = useState();
+  useEffect(() => {
+    getSession().then((session) => {
+      // setLoadedSession(session);
+
+      if (!session) {
+        window.location.href = "/auth";
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
   const [session, loading] = useSession();
 
-  if (loading) {
-    return <p className={classes.profile} >Loading...</p>;
+  if (isLoading) {
+    return <p className={classes.profile}>Loading...</p>;
   }
 
   return (

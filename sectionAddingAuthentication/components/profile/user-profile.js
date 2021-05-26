b12a43820450e1b5442fd862b65939acd3cb5198
NelsonWang <wangnelson2@gmail.com>
2021-05-26 15:04:23 -0400

    section13 auth | install | lib db | user profile

--- sectionAddingAuthentication/components/profile/user-profile.js
+++ sectionAddingAuthentication/components/profile/user-profile.js
@@ -1,8 +1,15 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import { useSession } from "next-auth/client";
+
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
 
 function UserProfile() {
   // Redirect away if NOT auth
+  const [session, loading] = useSession();
+
+  if (loading) {
+    return <p className={classes.profile} >Loading...</p>;
+  }
 
   return (
     <section className={classes.profile}>

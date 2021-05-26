074fa5c72aa98d4a9fe29df148cd65551a12cc13
NelsonWang <wangnelson2@gmail.com>
2021-05-26 15:36:54 -0400

    section13 auth | install | lib db | auth

--- sectionAddingAuthentication/pages/auth.js
+++ sectionAddingAuthentication/pages/auth.js
@@ -1,6 +1,24 @@
-import AuthForm from '../components/auth/auth-form';
+import router, { useRouter } from "next/router";
+import { getSession } from "next-auth/client";
+import { useEffect } from "react";
+
+import AuthForm from "../components/auth/auth-form";
 
 function AuthPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace("/");
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   return <AuthForm />;
 }
 

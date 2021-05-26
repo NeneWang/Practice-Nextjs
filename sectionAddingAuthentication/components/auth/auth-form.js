13a2b9710b0843904c3175300fc1e3ad2a0b3a6e
NelsonWang <wangnelson2@gmail.com>
2021-05-26 13:51:16 -0400

    section13 auth | install | lib db | auth-form

--- sectionAddingAuthentication/components/auth/auth-form.js
+++ sectionAddingAuthentication/components/auth/auth-form.js
@@ -1,20 +1,21 @@
-import { useState, useRef } from 'react';
+import { useState, useRef } from "react";
+import { signIn } from "next-auth/client";
 
-import classes from './auth-form.module.css';
+import classes from "./auth-form.module.css";
 
 async function createUser(email, password) {
-  const response = await fetch('/api/auth/signup', {
-    method: 'POST',
+  const response = await fetch("/api/auth/signup", {
+    method: "POST",
     body: JSON.stringify({ email, password }),
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   });
 
   const data = await response.json();
 
   if (!response.ok) {
-    throw new Error(data.message || 'Something went wrong!');
+    throw new Error(data.message || "Something went wrong!");
   }
 
   return data;
@@ -40,6 +41,14 @@ function AuthForm() {
 
     if (isLogin) {
       // log user in
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword,
+      });
+
+      console.log(result);
+
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
@@ -52,29 +61,29 @@ function AuthForm() {
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required ref={emailInputRef} />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
+          <label htmlFor="password">Your Password</label>
           <input
-            type='password'
-            id='password'
+            type="password"
+            id="password"
             required
             ref={passwordInputRef}
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
--- sectionAddingAuthentication/pages/api/auth/[...nextauth].js
+++ sectionAddingAuthentication/pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+
+
 import { verifyPassword } from "../../../lib/auth";
 import { connectToDatabase } from "../../../lib/db";
 

7d21980f70d187ba1712207d64bfa63b8a35d688
NelsonWang <wangnelson2@gmail.com>
2022-12-01 16:39:42 -0500

    updated pass

--- sectionAddingAuthentication/lib/db.js
+++ sectionAddingAuthentication/lib/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export async function connectToDatabase() {
   const client = await MongoClient.connect(
-    "mongodb+srv://nelson:1223@cluster0.kzhr5.mongodb.net/auth-demo?retryWrites=true&w=majority"
+    "mongodb+srv://nelson_pass123:pass123@cluster0.kzhr5.mongodb.net/auth-demo?retryWrites=true&w=majority"
   );
   return client;
 }
